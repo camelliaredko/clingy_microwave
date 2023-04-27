16b355ee2934092c89f35a54786b6449ce560635
Camellia Redko <camelliaredko@gmail.com>
2023-04-27 18:44:36 +0200

    All basic parts work

--- anxiously_attached_microwave.ino.ino
+++ anxiously_attached_microwave.ino.ino
@@ -17,11 +17,13 @@
 #define NEOPIXEL_PIN 7
 #define NUM_PIXELS 24
 
-Adafruit_NeoPixel ring(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-Servo myservo;
+Adafruit_NeoPixel neoPixelRing = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Servo servoMotor;
 
 int buttonPressCount = 0;
 int condClose = 0;
+const int minAngle = 3; // Servo minAngle
+const int maxAngle = 87; // Servo maxAngle
 
 Bounce button1 = Bounce(); // Instantiate a Bounce object
 
@@ -33,10 +35,8 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
-  myservo.attach(SERVO_PIN);
-  ring.begin();
-  ring.setBrightness(50);
-  ring.show();
+  servoMotor.attach(SERVO_PIN);
+  neoPixelRing.begin();
 }
 
 void loop() {
@@ -60,4 +60,52 @@ void loop() {
      buttonPressCount++;
     //  Serial.println(buttonPressCount); // Tests button presses
    }
+
+
+
+
+
+  // BLINKING RED NEOPIXEL RING: 
+  // Turn on all NeoPixels to red
+  neoPixelRing.fill(neoPixelRing.Color(255, 0, 0)); // Red color
+  neoPixelRing.show();
+
+  // Delay for the desired duration (in milliseconds)
+  delay(500); // 500ms (half a second)
+
+  // Turn off all NeoPixels
+  neoPixelRing.clear();
+  neoPixelRing.show();
+
+  // Delay before starting the next blink
+  delay(500); // 500ms (half a second)
+
+
+
+
+
+
+  // Move servo to minAngle
+  servoMotor.write(minAngle);
+  delay(1000);
+
+  // Move servo to maxAngle
+   servoMotor.write(maxAngle);
+  delay(1000);
+
+
+
+  // Play descending tones quietly
+  for (int frequency = 1000; frequency >= 200; frequency -= 100) {
+    analogWrite(BUZZER_PIN, 1);  // Set buzzer volume to a low value (adjust as needed)
+    tone(BUZZER_PIN, frequency, 250);  // Play the tone for 250 milliseconds
+    delay(300);  // Delay between each tone
+    noTone(BUZZER_PIN);  // Stop the tone
+    delay(20); // Delay between each tone to reduce overlap
+  }
+  
+  delay(1000);  // Delay before starting the sequence again
+
+
+
 }
